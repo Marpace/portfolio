8e79075989cec83463a47c9a3d8e9bda29d0a064
Marpace <almonacid.pablo@hotmail.com>
2022-07-25 22:57:19 -0400

    update resume

--- resume.js
+++ resume.js
@@ -3,16 +3,20 @@ const qsa = (selector) => document.querySelectorAll(selector);
 
 const pageNumberSelector = qs(".page-number__selector")
 const pageNumbers = Array.from(qsa(".page-number"));
+const resumePage2 = qs(".resume-2");
 
 pageNumbers.forEach(number => {
     number.addEventListener('click', () => {
         if(number.innerHTML === "1") {
-            pageNumberSelector.style.left = "-7px"
+            pageNumberSelector.style.left = "-7px";
+            resumePage2.style.opacity = "0"
         } else {
             pageNumberSelector.style.left = "35px"
+            resumePage2.style.opacity = "1"
         }
         pageNumbers.forEach(number => {
-            number.classList.toggle("number-active")
+            number.classList.remove("number-active");
         });
+        number.classList.add("number-active");
     });
 });
